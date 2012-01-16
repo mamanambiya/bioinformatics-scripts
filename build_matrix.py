085c09c61c440a69cb2292fa5a7c27aa3504a7f6
Steve Moss <gawbul@gmail.com>
2012-01-16 12:50:50 +0000

    Added build_matrix.py script

---
+++ build_matrix.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+	Build matrix - creates a matrix of a given number of dimensions and a given
+	number of rows and columns in length
+"""
+
+# module imports
+import sys, os, re, time
+
+# set start time
+start_time = time.time()
+
+# get arguments
+args = sys.argv[1:]
+
+# check args
+if len(args) < 2:
+	print "Requires matrix row length and column length arguments (+1 as zero-base indexing)"
+	sys.exit()
+
+# define variables
+col_length = int(args[0])
+row_length = int(args[1])
+matrix = []
+
+# create range for first row
+row_values = range(row_length)
+matrix.append(list(row_values))
+
+# do the groovy stuff
+count = 0
+while count < row_length - 1:
+	# adjust row values
+	first = count + 1
+	for i in range(row_length - 1, 0, -1):
+		new_values = row_values
+		new_values[i] = row_values[i - 1]
+	new_values[0] = first
+	matrix.append(list(new_values))
+	# increment 
+	count += 1
+
+# save matrix to a file
+matrixfile = open("matrix.txt", "w")
+for row in matrix:
+	matrixfile.write(' '.join([str(r) for r in row]) + "\n")
+matrixfile.close()
